98afce5c3e15d4e70259dbaebad36ac13e8e24e8
Jan <krimdomu@googlemail.com>
2012-10-21 10:47:47 +0200

    query server

--- lib/Rex/IO/WebUI/Server.pm
+++ lib/Rex/IO/WebUI/Server.pm
@@ -3,6 +3,10 @@ use Mojo::Base 'Mojolicious::Controller';
 
 sub index {
    my $self = shift;
+
+   my $server = $self->rexio->get_server($self->param("id"));
+   $self->stash("server", $server);
+
    $self->render;
 }
 
