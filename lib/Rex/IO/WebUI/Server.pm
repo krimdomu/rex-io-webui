3b63f6cc61f76caf822df67384c8e3f8abbaaba2
Jan <krimdomu@googlemail.com>
2012-10-27 15:28:58 +0200

    added operating system and current boottype

--- lib/Rex/IO/WebUI/Server.pm
+++ lib/Rex/IO/WebUI/Server.pm
@@ -5,7 +5,10 @@ sub index {
    my $self = shift;
 
    my $server = $self->rexio->get_server($self->param("id"));
+   my $os_templates = $self->rexio->list_os_templates;
+
    $self->stash("server", $server);
+   $self->stash("os_templates", $os_templates);
 
    $self->render;
 }
