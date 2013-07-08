b4ba3aaaca3662fb6ed47afa1d4af138c8ab531b
Jan <krimdomu@googlemail.com>
2013-07-08 20:53:18 +0200

    fixed routing for services

--- lib/Rex/IO/WebUI/Server.pm
+++ lib/Rex/IO/WebUI/Server.pm
@@ -283,7 +283,7 @@ sub rexio_routes {
    $r_auth->post("/server_group/server/:server_id/:group_id")->to("server#add_server_to_group");
 
    # set next boot // todo: andere url
-   $r_auth->post("/server/:server/:boot")->to("server#set_next_boot");
+   $r_auth->post("/server/:server/boot/:boot")->to("server#set_next_boot");
    $r_auth->route("/server/#ip/command")->via("RUN")->to("server#run_command");
 }
 
