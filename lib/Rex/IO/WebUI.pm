4768bd08e2f9ee6e98cb7ac8f4ab1c1f7b963cbe
Jan <krimdomu@googlemail.com>
2013-10-30 17:58:17 +0100

    version update

--- lib/Rex/IO/WebUI.pm
+++ lib/Rex/IO/WebUI.pm
@@ -5,7 +5,7 @@ use File::Basename 'dirname';
 use File::Spec::Functions 'catdir';
 use Cwd 'getcwd';
 
-our $VERSION = "0.2.19";
+our $VERSION = "0.2.20";
 
 # This method will run once at server start
 sub startup {
