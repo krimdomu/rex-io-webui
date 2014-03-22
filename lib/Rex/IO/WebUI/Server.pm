82cf60bd41ccef1f62e9fe819998f3c1548ccdaf
Jan <krimdomu@googlemail.com>
2014-03-22 17:11:34 +0100

    moved out inventory, added some basic js objects

--- lib/Rex/IO/WebUI/Server.pm
+++ lib/Rex/IO/WebUI/Server.pm
@@ -307,74 +307,7 @@ sub add_server_to_group {
   $self->render(json => $ret);
 }
 
-################################################################################
-# Network Adapter functions
-################################################################################
-sub get_network_adapter {
-  my ($self) = @_;
 
-  my $ref = $self->rexio->call(GET => "1.0", "hardware", hardware => $self->param("server_id"), network_adapter => $self->param("network_adapter_id"));
-
-  if(exists $ref->{data}) {
-    return $self->render(json => $ref->{data});
-  }
-
-  $self->render(json => $ref);
-}
-
-sub update_network_adapter {
-  my ($self) = @_;
-
-  $self->app->log->debug("Updating network adapter: " . $self->param("network_adapter_id"));
-  $self->app->log->debug(Dumper($self->req->json));
-
-  my $ref = $self->rexio->call(POST => "1.0", "hardware", hardware => $self->param("server_id"), network_adapter => $self->param("network_adapter_id"), ref => $self->req->json);
-
-  $self->render(json => $ref);
-}
-
-sub delete_network_adapter {
-  my ($self) = @_;
-
-  my $ref = $self->rexio->call(DELETE => "1.0", "hardware", hardware => $self->param("server_id"), network_adapter => $self->param("network_adapter_id"));
-
-  $self->render(json => $ref);
-}
-
-################################################################################
-# Bridge functions
-################################################################################
-sub update_bridge {
-
-  my ($self) = @_;
-
-  $self->app->log->debug("Updating bridge: " . $self->param("bridge_id"));
-  $self->app->log->debug(Dumper($self->req->json));
-
-  my $ref = $self->rexio->call(POST => "1.0", "hardware", hardware => $self->param("server_id"), bridge => $self->param("bridge_id"), ref => $self->req->json);
-
-  $self->render(json => $ref);
-}
-
-sub delete_bridge {
-  my ($self) = @_;
-
-  my $ref = $self->rexio->call(DELETE => "1.0", "hardware", hardware => $self->param("server_id"), bridge => $self->param("bridge_id"));
-
-  $self->render(json => $ref);
-}
-
-sub get_bridge {
-  my ($self) = @_;
-
-  my $ref = $self->rexio->call(GET => "1.0", "hardware", hardware => $self->param("server_id"), bridge => $self->param("bridge_id"));
-
-  if(exists $ref->{data}) {
-    return $self->render(json => $ref->{data});
-  }
-
-  $self->render(json => $ref);
-}
 
 ##### Rex.IO WebUI Plugin specific methods
 sub rexio_routes {
@@ -404,13 +337,6 @@ sub rexio_routes {
   $r_auth->post("/server/:server/boot/:boot")->to("server#set_next_boot");
   $r_auth->route("/server/#ip/command")->via("RUN")->to("server#run_command");
 
-  $r_auth->get("/server/:server_id/network_adapter/:network_adapter_id")->to("server#get_network_adapter");
-  $r_auth->post("/server/:server_id/network_adapter/:network_adapter_id")->to("server#update_network_adapter");
-  $r_auth->delete("/server/:server_id/network_adapter/:network_adapter_id")->to("server#delete_network_adapter");
-
-  $r_auth->get("/server/:server_id/bridge/:bridge_id")->to("server#get_bridge");
-  $r_auth->post("/server/:server_id/bridge/:bridge_id")->to("server#update_bridge");
-  $r_auth->delete("/server/:server_id/bridge/:bridge_id")->to("server#delete_bridge");
 
 }
 
